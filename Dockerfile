c048c7bbec8f0750623b81371134a35a3db308a1
Bradley Skaggs <bskaggs@acm.org>
2019-03-05 23:49:40 -0500

    Ghidra that runs within the memory bounds set by a cgroup

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+ARG BASE_IMAGE=openjdk:11-slim
+FROM ${BASE_IMAGE}
+
+ARG GHIDRA_VERSION=9.0_PUBLIC_20190228
+WORKDIR /opt
+RUN apt-get update && apt-get install -y wget gettext-base && \
+    wget -q -O ghidra.zip https://ghidra-sre.org/ghidra_${GHIDRA_VERSION}.zip && \
+    unzip ghidra.zip && \
+    rm ghidra.zip && \ 
+    ln -s ghidra* ghidra && \
+    useradd --system --shell /bin/false ghidra && \
+    mkdir -p /srv/repositories && \
+    chown -R ghidra: ghidra* /srv/repositories
+USER ghidra
+VOLUME /srv/repositories
+WORKDIR /opt/ghidra
+ENV ghidra_home=/opt/ghidra
+COPY server.sh /opt/ghidra
+EXPOSE 13100
+EXPOSE 13101
+EXPOSE 13102
+CMD ["/opt/ghidra/server.sh"]
---
+++ server.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+GHIDRA_CLASSPATH="$(sed 's/.*\(${ghidra_home}\)/\1/g' "${ghidra_home}/Ghidra/Features/GhidraServer/data/classpath.frag" | sed ':a;N;$!ba;s/\n/:/g'| envsubst )"
+: ${GHIDRA_REPOSITORIES_PATH:=/srv/repositories}
+#create the first user on startup, if users file doesn't exist
+if [ ! -e "${GHIDRA_REPOSITORIES_PATH}/users" ] && [ ! -z "${GHIDRA_DEFAULT_USER}" ]; then
+  echo "Creating user ${GHIDRA_DEFAULT_USER} with default password 'changeme'"
+  mkdir -p "${GHIDRA_REPOSITORIES_PATH}/~admin"
+  echo "-add ${GHIDRA_DEFAULT_USER}" > "${GHIDRA_REPOSITORIES_PATH}/~admin/adm.cmd"
+fi
+exec java \
+  -classpath "${GHIDRA_CLASSPATH}" \
+  -Djava.net.preferIPv4Stack=true \
+  -DApplicationRollingFileAppender.maxBackupIndex=10 \
+  -Dclasspath_frag="${ghidra_home}/Ghidra/Features/GhidraServer/data/classpath.frag" \
+  -Ddb.buffers.DataBuffer.compressedOutput=true \
+  -Djava.library.path="${ghidra_home}/Ghidra/Features/GhidraServer/os/linux64" \
+  -XX:InitialRAMPercentage=10 \
+  -XX:MinRAMPercentage=10 \
+  -XX:MaxRAMPercentage=80 \
+  -Djava=/usr/bin/java \
+  -Dos_dir="${ghidra_home}/Ghidra/Features/GhidraServer/os/linux64" \
+  -Dghidra_home="${ghidra_home}" \
+  -Djna_tmpdir=/tmp \
+  ghidra.server.remote.GhidraServer \
+  -a0 \
+  "${GHIDRA_REPOSITORIES_PATH}"
+  
+  #-ip"$(hostname -I)" \
